2e68caf208d5eaf775b4aa4b0c7b130b86326760
emperorhan <iny27@naver.com>
2019-01-08 19:03:24 +0900

    Fix errors

--- cleos/main.cpp
+++ cleos/main.cpp
@@ -975,7 +975,7 @@ struct register_producer_subcommand {
             producer_key = public_key_type(producer_key_str);
          } EOS_RETHROW_EXCEPTIONS(public_key_type_exception, "Invalid producer public key: ${public_key}", ("public_key", producer_key_str))
 
-         auto regprod_var = regproducer_variant(producer_str, producer_key, max_supply, url, loc );
+         auto regprod_var = regproducer_variant(producer_str, producer_key, max_supply, transfer_ratio, url, loc );
          auto accountPermissions = get_account_permissions(tx_permission, {producer_str,config::active_name});
          send_actions({create_action(accountPermissions, config::system_account_name, N(regproducer), regprod_var)});
       });
@@ -1005,7 +1005,7 @@ struct update_producer_subcommand {
             producer_key = public_key_type(producer_key_str);
          } EOS_RETHROW_EXCEPTIONS(public_key_type_exception, "Invalid producer public key: ${public_key}", ("public_key", producer_key_str))
 
-         auto updateprod_var = updateproducer_variant(producer_str, producer_key, url, loc );
+         auto updateprod_var = updateproducer_variant(producer_str, producer_key, transfer_ratio, url, loc );
          auto accountPermissions = get_account_permissions(tx_permission, {producer_str,config::active_name});
          send_actions({create_action(accountPermissions, config::system_account_name, N(updateproducer), updateprod_var)});
       });
