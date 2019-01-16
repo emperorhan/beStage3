7e3c471fce0059115062ccb479108382d5cacebf
emperorhan <iny27@naver.com>
2019-01-16 17:25:30 +0900

    Fix string err

--- eosio.system/eosio.system.hpp
+++ eosio.system/eosio.system.hpp
@@ -271,7 +271,7 @@ namespace eosiosystem {
 
          // functions defined in voting.cpp
 
-         void regproducer( const account_name producer, const eosio::public_key& producer_key, int64_t amount, string sym, double transfer_ratio, const std::string& url, uint16_t location );
+         void regproducer( const account_name producer, const eosio::public_key& producer_key, int64_t amount, std::string sym, double transfer_ratio, const std::string& url, uint16_t location );
 
          void updateprod( const account_name producer, const public_key& producer_key, double transfer_ratio, const std::string& url, uint16_t location );
 
